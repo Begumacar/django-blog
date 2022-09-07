75598dd651fa4e6aa836171460fdd9e94dd9bfb4
Begum Acar <begumacaar@gmail.com>
2022-09-08 00:54:42 +0300

    iletişim sayfası yapıldı.

--- config/settings.py
+++ config/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'account',
     'blog',
     #third party
-    'ckeditor'
+    'ckeditor',
+    'crispy_forms',
     
 ]
 
@@ -134,5 +135,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL='/media/'
 MEDIA_ROOT= os.path.join(BASE_DIR, 'media/')
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 
