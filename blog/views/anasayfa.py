f83e0594ea3603b424528ab5f9b4e595f768727d
Begum Acar <begumacaar@gmail.com>
2022-09-07 15:50:24 +0300

    Listeleme ve Sayfalama kısmı bitti.

--- blog/views/anasayfa.py
+++ blog/views/anasayfa.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from blog.models import YazilarModel
+from django.core.paginator import Paginator
 
 def anasayfa(request):
-    context={
-        'isim': 'Begüm Acar'
-    }
-    return render(request, 'pages/anasayfa.html', context=context)
+    yazilar = YazilarModel.objects.order_by('-id')
+    sayfa =  request.GET.get('sayfa')
+    paginator = Paginator(yazilar, 1)
+    
+    
+    
+    return render(request, 'pages/anasayfa.html', context={
+        'yazilar': paginator.get_page(sayfa)       
+    })
