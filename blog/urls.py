e05cf1549facf13bc9b3ac85a57c9e4434dabb98
Begum Acar <begumacaar@gmail.com>
2022-09-09 00:34:44 +0300

    Yazı Güncelle tamamlandı.

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from blog.views import iletisim, anasayfa, kategori, yazilarim, detay
+from blog.views import iletisim, anasayfa, kategori, yazilarim, detay, yazi_ekle, yazi_guncelle
 
 urlpatterns = [
     path('', anasayfa, name='anasayfa'),
@@ -7,4 +7,6 @@ urlpatterns = [
     path('kategori/<slug:kategoriSlug>', kategori, name='kategori'),
     path('yazilarim', yazilarim, name='yazilarim'),
     path('detay/<slug:slug>', detay, name='detay'),
-]
+    path('yazi-ekle', yazi_ekle, name='yazi-ekle'),
+    path('yazi-guncelle/<slug:slug>', yazi_guncelle, name='yazi-guncelle'),
+] 
