86fcaecac706ba318733414be9a591d0ee271e1d
Begum Acar <begumacaar@gmail.com>
2022-09-15 01:28:19 +0300

    S3 ayarları yapıldı

--- config/settings/base.py
+++ config/settings/base.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     #third party
     'ckeditor',
     'crispy_forms',
+    'storages'
     
 ]
 
@@ -156,15 +157,3 @@ LOGGING = {
 
 
 
-
-import sentry_sdk
-from sentry_sdk.integrations.django import DjangoIntegration
-
-sentry_sdk.init(
-    dsn=env('SENTRY_DSN'),
-    integrations=[
-        DjangoIntegration(),
-    ],
-    traces_sample_rate=1.0,
-    send_default_pii=True
-)
